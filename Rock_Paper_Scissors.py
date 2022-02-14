4b153825cd9039ce4d6f981f5d51a8071453cb76
Dhananjana94 <nadunwanninayaka@gmail.com>
2022-02-14 20:20:42 +0530

    Code completed

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -3,10 +3,10 @@ import random
 user_wins = 0
 computer_wins = 0
 
-options = ["Rock","Paper","Scissor"]
+options = ["rock","paper","scissor"]
 
 while True:
-    user_input = input("Type Rock/Paper/Scissor or q to quit the game ").lower()
+    user_input = input("Type Rock/Paper/Scissor or q to quit the game: ").lower()
 
     if user_input == "q":
         break
@@ -20,5 +20,26 @@ while True:
     computer_picked = options[random_number]
     print("Computer picked",computer_picked)
 
+    if user_input =="rock" and computer_picked == "scissor":
+        print("You Won")
+        user_wins +=1
 
+
+
+    elif user_input =="paper" and computer_picked == "rock":
+        print("You Won")
+        user_wins +=1
+
+
+
+    elif user_input =="scissor" and computer_picked == "paper":
+        print("You Won")
+        user_wins +=1
+
+    else:
+        print("You Lost!")
+        computer_wins +=1
+
+print("You wins",user_wins,"times.")
+print("Computer wins",computer_wins,"times.")
 print("GoodBye!")
